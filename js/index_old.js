d961f8a9c9915b95e737aaccc2690e3ee85bc4ef
darkwing17 <echu916@gmail.com>
2019-08-27 23:13:00 -0700

    Initial

---
+++ js/index_old.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+
+    // On load, run the headshot loader.
+    var ImageId = 1;
+    var numberofImages = 4;
+
+    window.setInterval(function () {
+        $('.big_banner_image').fadeTo('slow', 0, function () {
+            $(this).css('background-image', 'url(img/' + ImageId + '.png').fadeTo('slow', 1);
+        });
+        ImageId = (ImageId + 1) % numberofImages;
+
+    }, 4 * 1000);
+
+
+    // Set up a changing mouseover color for each of the buttons.
+    $('li').hover(
+        function() {
+            $(this).css('background-color', '#2543cc');
+            $(this).css('border-top', '1px solid #2543cc');
+            $(this).css('border-bottom', '1px solid #2543cc');
+
+        },
+        function() {
+            $(this).css('background-color', 'orange');
+            $(this).css('border-top', '1px solid orange');
+            $(this).css('border-bottom', '1px solid orange');
+
+        }
+    );
+
+    // Set up listeners for each of the buttons
+    $("#home").on('click,', noContentAvailable);
+    $("#aboutme").on('click', noContentAvailable);
+    $("#folio").on('click', noContentAvailable);
+    $("#CV_resume").on('click', noContentAvailable);
+
+});
+
+// The plan is to -- most likely -- use the links as if they were buttons,
+// and load the content into a JavaScript driven window.
+//
+
+function noContentAvailable()
+{
+
+    // No Content Available warning message
+    var noContentAvail = '<div class=\"header\"><a id=\"close\" href="#">close X</a></div>';
+    noContentAvail += 'Link not active at this time.</div>';
+
+    // Create content for No Content Available message
+    var elNote = document.createElement('div');
+    elNote.setAttribute('id', 'note');
+    elNote.innerHTML = noContentAvail;
+    document.body.appendChild(elNote);
+
+    // Closes No Content Available
+    function dismissNote() {
+        document.body.removeChild(elNote);
+    }
+
+    var elClose = document.getElementById('close');
+    $(elClose).on('click', dismissNote);
+}
+
+function returnToHome(){}
+function aboutme(){}
+function folio(){}
+function CV_resume(){}
